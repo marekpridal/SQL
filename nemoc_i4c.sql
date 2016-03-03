eea9d1dfb6064793ac34fa525cbd8fb5ed0000f4
marekpridal <m.pridal29@gmail.com>
2016-03-03 16:10:27 +0100

    Revize 3.3.2016
    - přidání engine
    - přidání on delete cascade

--- nemoc_i4c.sql
+++ nemoc_i4c.sql
@@ -1,65 +1,55 @@
-create database  nemoc_I4C default character set utf8 collate utf8_czech_ci;
+create database nemoc_i4c default char set utf8 collate utf8_czech_ci;
 use nemoc_i4c;
 
 create table lekar
 (
-		id_lekar int not null primary key auto_increment,
-        jmeno varchar(20) not null,
-        prijmeni varchar(20) not null,
-        mesto varchar(20) not null,
-        ulice varchar(20) not null,
-        ordinace varchar(10) not null,
-        telefon varchar(9) not null
-);
-
-show create table lekar;
-
-create table pacient
-(
-		id_pacient int not null primary key auto_increment,
-        jmeno varchar(10) not null,
-        prijmeni varchar(10) not null,
-        mesto varchar(20) not null,
-        ulice varchar(20) not null,
-        rodne_cislo int not null,
-        telefon varchar(9) not null
-);
+	id_lekar int auto_increment not null primary key,
+    jmeno varchar(10) not null,
+    prijmeni varchar(10) not null,
+    mesto varchar(10) not null,
+    ulice varchar(10) not null,
+    ordinace varchar(10) not null,
+    telefon varchar(10) not null
+)engine=InnoDB;
 
 create table pojistovna
 (
-		id_poj varchar(4) not null primary key,
-        nazev varchar(20) not null,
-        mesto varchar(20) not null,
-        ulice varchar(20) not null,
-        telefon varchar(9) not null
-);
+	id_poj varchar(4) not null primary key,
+    nazev varchar(10) not null,
+    mesto varchar(10) not null,
+    ulice varchar(20) not null,
+    telefon varchar(9) not null
+)engine=InnoDB;
 
-/*drop table doktor;*/
-
-alter table pacient add id_poj varchar(4) not null;
-
-alter table pacient add constraint FK_pac_poj foreign key(id_poj) references pojistovna(id_poj);
+create table pacient
+(
+	id_pacient int not null auto_increment primary key,
+    jmeno varchar(10) not null,
+    prijmeni varchar(10) not null,
+    mesto varchar(10) not null,
+    ulice varchar(10) not null,
+    rodne_cislo varchar(11) not null,
+    telefon varchar(9) not null,
+    id_poj varchar(4) not null,
+    constraint foreign key FK_pojistovna_pacient (id_poj) references pojistovna (id_poj) on delete cascade
+)engine=InnoDB;
 
 create table navsteva
 (
 	id_lekar int not null,
-	id_pacient int not null,
+    id_pacient int not null,
+    primary key(id_lekar,id_pacient),
     datum date not null,
     duvod varchar(50),
-    primary key(id_lekar,id_pacient,datum),
-	constraint FK_pac_nav foreign key(id_pacient) references pacient(id_pacient),
-	constraint FK_lek_nav foreign key(id_lekar) references lekar(id_lekar)    
-);
+    constraint foreign key FK_pacient_navsteva (id_pacient) references pacient(id_pacient) on delete cascade,
+    constraint foreign key FK_lekar_navsteva (id_lekar) references lekar(id_lekar) on delete cascade
+)engine=InnoDB;
 
 create table lek_poj
 (
 	id_lekar int not null,
     id_poj varchar(4) not null,
     primary key(id_lekar,id_poj),
-    constraint FK_lek_poj foreign key (id_lekar) references lekar(id_lekar),
-    constraint FK_poj_lek foreign key (id_poj) references pojistovna(id_poj)
-);
-
-/*alter table lekar add zamereni varchar(15) not null;
-
-alter table lekar drop column zamereni;*/
+    constraint foreign key FK_lek_poj (id_lekar) references lekar(id_lekar) on delete cascade,
+    constraint foreign key FK_poj_lek (id_poj) references pojistovna(id_poj) on delete cascade
+)engine=InnoDB;
